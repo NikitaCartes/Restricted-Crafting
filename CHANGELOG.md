52b10f101470c85215942227b1137da13d8bfb67
NikitaCartes <nikich98@yandex.ru>
2025-01-22 00:27:36 +0100

    Init version

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+### 0.1.0
+##### Minecraft 1.21.1
+
+1) Restrict crafting based on permissions `restricted-crafting.<minecraft item>`, e.g. `restricted-crafting.minecraft:iron_ingot`
---
+++ src/main/java/xyz/nikitacartes/restrictedcrafting/RestrictedCrafting.java
@@ -0,0 +1,10 @@
+package xyz.nikitacartes.restrictedcrafting;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RestrictedCrafting implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
